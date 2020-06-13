d34853b3b4a22cfec552c02b343de275a3dffc37
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-06-12 23:18:43 -0700

    Update open.py

--- open.py
+++ open.py
@@ -3,3 +3,11 @@ import re
 import sys
 import xlwt
 from xlwt import Workbook 
+
+if len(sys.argv)!=3:
+    print("Usage: python open.py [input_file.json] [output_file.xls]")
+    sys.exit(1)
+
+input_file = sys.argv[1]
+
+output_file = sys.argv[2]
